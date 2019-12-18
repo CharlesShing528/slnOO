2b007ac4af40538e2f9d1e8830934c075f48f838
Charles4982 <Charles4982@startravel.com.tw>
2019-12-19 00:44:24 +0800

    建構函試多載

---
+++ EXConstructor_02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXConstructor_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            Console.WriteLine("矩形一 採 Rectangle() 預設建構函式…");
+            Console.WriteLine("長度：{0}，寬度：{1}，面積：{2}",rect1.Length,rect1.Width,rect1.GetArea());
+            Console.WriteLine();
+
+            Rectangle rect2 = new Rectangle(12);
+            Console.WriteLine("矩形二 採 Rectangle(12) 建構函式…");
+            Console.WriteLine("長度：{0}，寬度：{1}，面積：{2}", rect2.Length, rect2.Width, rect2.GetArea());
+            Console.WriteLine();
+
+            Rectangle rect3 = new Rectangle(15,10);
+            Console.WriteLine("矩形二 採 Rectangle(15,10) 建構函式…");
+            Console.WriteLine("長度：{0}，寬度：{1}，面積：{2}", rect3.Length, rect3.Width, rect3.GetArea());
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
---
+++ EXConstructor_02/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXConstructor_02
+{
+    class Rectangle
+    {
+        public int Length { get; set; }// Length 為長度屬性
+        public int Width { get; set; }// Width 為寬度屬性
+
+        public Rectangle()      // 沒有引數的 Default Constructor
+        {
+            Length = 10;
+            Width = 10;
+        }
+
+        public Rectangle(int L)      // 一個長度的引數的建構函式 Constructor
+        {
+            Length = L;
+            Width = 10;
+        }
+
+        public Rectangle(int L, int W)   // 含有長和寬引數的建構函式 Constructor
+        {
+            Length = L;
+            Width = W;
+        }
+
+        public int GetArea()
+        {
+            return Length * Width;
+        }
+    }
+}
