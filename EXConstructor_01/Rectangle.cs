dc592896591ee4f1bee4a923a03fa17dc5812465
Charles4982 <Charles4982@startravel.com.tw>
2019-12-19 00:37:44 +0800

    使用建構函式

--- EXConstructor_01/Rectangle.cs
+++ EXConstructor_01/Rectangle.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// 建構函式是類別在建立物件實體時自動在幕後執行的方法方員。
+/// 當類別使用 new 觮立該類別物件的實體時，立即執行和類別同名的建構函式。
+/// 建構函式不允許傳回值
+/// </summary>
 namespace EXConstructor_01
 {
     class Rectangle
