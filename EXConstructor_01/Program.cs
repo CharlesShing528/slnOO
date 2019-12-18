17533c2c385b0f71ce09cb467dc28e8f9edb7f52
Charles4982 <Charles4982@startravel.com.tw>
2019-12-19 00:35:34 +0800

    使用建構函式

---
+++ EXConstructor_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXConstructor_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            Console.WriteLine();
+            Rectangle rect2 = new Rectangle(15,10);
+            Console.WriteLine("矩形長度：{0}",rect2.Length);
+            Console.WriteLine("矩形寬度：{0}", rect2.Width);
+            Console.WriteLine("矩形面積：{0}", rect2.GetArea());
+            Console.Read();
+        }
+    }
+}
---
+++ EXConstructor_01/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXConstructor_01
+{
+    class Rectangle
+    {
+        public int Length { get; set; }// Length 為長度屬性
+        public int Width { get; set; }// Width 為寬度屬性
+
+        public Rectangle()  // 沒引數的 Default Constructor
+        {
+            Console.WriteLine("已進入 Default Constructor ...");
+        }
+
+        public Rectangle(int L, int W)  //含有長和寬引數的建構函式
+        {
+            Length = L;
+            Width = W;
+        }
+
+        public int GetArea()
+        {
+            return Length * Width;
+        }
+    }
+}
