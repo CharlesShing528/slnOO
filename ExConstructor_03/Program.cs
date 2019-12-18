d00386e3ffe4678e67a4b8687b78a02d138a4790
Charles4982 <Charles4982@startravel.com.tw>
2019-12-19 00:50:13 +0800

    建構式，物件初始設定式

---
+++ ExConstructor_03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExConstructor_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle{ Length = 15, Width = 10 };
+            Console.WriteLine("矩形長度：{0}", rect1.Length);
+            Console.WriteLine("矩形寬度：{0}", rect1.Width);
+            Console.WriteLine("矩形面積：{0}", rect1.GetArea());
+            Console.WriteLine();
+
+            Rectangle rect2 = new Rectangle { Length= 12 };
+            Console.WriteLine("矩形長度：{0}", rect2.Length);
+            Console.WriteLine("矩形寬度：{0}", rect2.Width);
+            Console.WriteLine("矩形面積：{0}", rect2.GetArea());
+            Console.WriteLine();
+
+            Rectangle rect3 = new Rectangle();
+            Console.WriteLine("矩形長度：{0}", rect3.Length);
+            Console.WriteLine("矩形寬度：{0}", rect3.Width);
+            Console.WriteLine("矩形面積：{0}", rect3.GetArea());
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
---
+++ ExConstructor_03/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExConstructor_03
+{
+    class Rectangle
+    {
+        public int Length { get; set; }// Length 為長度屬性
+        public int Width { get; set; }// Width 為寬度屬性
+
+        public int GetArea()
+        {
+            return Length * Width;
+        }
+    }
+}
