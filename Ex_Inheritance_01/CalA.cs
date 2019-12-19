f6becd684c4d7212e9f6ae1b4d6971249cfb0263
Charles4982 <Charles4982@startravel.com.tw>
2019-12-20 00:36:32 +0800

    繼承 Inheritance

---
+++ Ex_Inheritance_01/CalA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Inheritance_01
+{
+    class CalA
+    {
+        // 可給繼承本類別的子類別繼承
+        protected int num1, num2;
+
+        public void SetValue(int n1,int n2)
+        {
+            num1 = n1;
+            num2 = n2;
+        }
+
+        public int GetSum()
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Ex_Inheritance_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Inheritance_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalA aa = new CalA();
+            int n1, n2;
+            n1 = 10; n2 = 15;
+            aa.SetValue(n1, n2);
+            Console.WriteLine("{0} + {1} = {2}",n1,n2,aa.GetSum());
+            Console.WriteLine("---------------------");
+            CalB bb = new CalB();
+            n1 = 20;n2 = 25;
+            bb.SetValue(n1, n2);
+            Console.WriteLine("{0} + {1} = {2}",n1,n2,bb.GetSum()   );
+            Console.WriteLine("{0} * {1} = {2}", n1, n2, bb.GetMult());
+            Console.Read();
+        }
+    }
+}
