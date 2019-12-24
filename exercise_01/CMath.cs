127e409bee146613c4bba904c08058ae3ff4f4fe
Charles4982 <Charles4982@startravel.com.tw>
2019-12-25 00:21:39 +0800

    建立 CMath類別，定義多載的 GetMin 方法可傳出兩整數的最小數， 也可以傳出整數陣列元素中的最小值。

---
+++ exercise_01/CMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_01
+{
+    class CMath
+    {
+        public int GetMin(int n1,int n2)
+        {
+            if (n1 > n2)
+                return n1;
+            else
+                return n2;
+        }
+
+        public int GetMin(int[] n1)
+        {
+            int min = n1[0];
+            for (int i = 1; i < n1.Length-1; i++)
+            {
+                if (min > n1[i])
+                    min = n1[i];
+            }
+            return min;
+        }
+    }
+}
---
+++ exercise_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CMath cMath = new CMath();
+            int a = -20;
+            int b = 15;
+            Console.WriteLine("{0}, {1} 較小值為 {2}",a,b,cMath.GetMin(a,b));
+            int[] c = { -10, 0, 30, -12, 25 };
+            string cValue = "";
+            foreach(var cv in c)
+            {
+                if (cValue != "")
+                    cValue += ",";
+                cValue += cv;
+            }
+            Console.WriteLine("{0} 中最小值為 {1}",cValue,cMath.GetMin(c));
+            Console.Read();
+        }
+    }
+}
